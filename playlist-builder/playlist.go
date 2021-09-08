1df4e9fb4c9742dfb63d59f9a104f3cb64554f16
Kevin Pierce <kevinpierce08@gmail.com>
2021-09-07 20:28:17 -0400

    Compiles entire list of Spotify URIs

--- playlist-builder/playlist.go
+++ playlist-builder/playlist.go
@@ -8,18 +8,34 @@ import (
 )
 
 func BuildPlaylist(client *spotify.Client, ctx context.Context, songList []string) {
+	fmt.Println(songList)
+	var uriList []spotify.URI
 	user, err := client.CurrentUser(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("You are logged in as:", user.ID)
 
-	result, err := client.Search(ctx, songList[0], spotify.SearchTypeTrack)
-	if err != nil {
-		log.Fatal(err)
+	for _, songName := range songList {
+		result, err := client.Search(ctx, songName, spotify.SearchTypeTrack)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(result.Tracks.Tracks) > 0 {
+			songURI := result.Tracks.Tracks[0].SimpleTrack.URI
+			uriList = append(uriList, songURI)
+		}
 	}
 
-	fmt.Println(result.Tracks.Tracks[0].SimpleTrack.URI)
+	fmt.Println(uriList)
+
+	// ///result, err := client.Search(ctx, songList[0], spotify.SearchTypeTrack)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(result.Tracks.Tracks[0].SimpleTrack.URI)
 
 	// newPlaylist, err := client.CreatePlaylistForUser(context.Background(), user.ID, "TEST GOLANG", "Test for my golang application", true, false)
 	// if err != nil {
